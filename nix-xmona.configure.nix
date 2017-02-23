041ba28895172040112a83a8a466c2426d3860f7
Haisheng, Wu <freizl@gmail.com>
2017-02-22 17:11:50 -0800

    nixos configure by enabling xmonad
    still fatal error when login...

--- nix-xmona.configure.nix
+++ nix-xmona.configure.nix
@@ -1,1 +1,31 @@
-aa
+{ config, pkgs, ... }:
+
+{
+  imports = [ <nixpkgs/nixos/modules/virtualisation/virtualbox-image.nix> ];
+  
+  system.autoUpgrade.enable = true;
+
+  networking.networkmanager.enable = true;
+
+  environment.systemPackages =
+  [ pkgs.vim
+    pkgs.emacs
+    pkgs.nodejs
+    pkgs.firefox
+    pkgs.git
+  ];
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.kdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.windowManager.xmonad.enable = true;
+  services.xserver.windowManager.xmonad.enableContribAndExtras = true;
+
+  users.extraUsers.alice =
+  { isNormalUser = true;
+    home = "/home/hw";
+    description = "Haisheng Wu";
+    extraGroups = [ "wheel" "users" "vboxsf" ];
+  };
+}
+
