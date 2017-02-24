2328ef5da76b635e2697f0cc19cbc51c1cc47424
Haisheng Wu <freizl@gmail.com>
2017-02-23 18:33:54 -0800

    an working version.

--- nix-xmonad.configure.nix
+++ nix-xmonad.configure.nix
@@ -1,25 +1,80 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ <nixpkgs/nixos/modules/virtualisation/virtualbox-image.nix> ];
-  
+
+
   system.autoUpgrade.enable = true;
 
   networking.networkmanager.enable = true;
+  virtualisation.virtualbox.guest.enable = true;
 
-  environment.systemPackages =
-  [ pkgs.vim
-    pkgs.emacs
-    pkgs.nodejs
-    pkgs.firefox
-    pkgs.git
+  environment.systemPackages = with pkgs;
+  [ vim
+    emacs
+    nodejs
+    firefox
+    git
+    rxvt_unicode
+    dmenu
+    #xmodmap
+    #gmrun
+    #xev
+    haskellPackages.xmobar
   ];
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.kdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
-  services.xserver.windowManager.xmonad.enable = true;
-  services.xserver.windowManager.xmonad.enableContribAndExtras = true;
+  fileSystems = [
+    { mountPoint = "/";
+      device = "/dev/disk/by-label/nixos";
+      label = "nixos";
+    }
+  ];
+
+  boot = {
+    initrd = {
+      # Disable journaling check on boot because virtualbox doesn't need it
+      checkJournalingFS = false;
+      # Make it pretty
+      kernelModules = [ "fbcon" ];
+    };
+    # Use the GRUB 2 boot loader.
+    loader.grub = {
+      enable = true;
+      #device = "/dev/disk/by-label/nixos";
+      device = "/dev/sda";
+    };
+  };
+
+  services =
+  {
+    xserver =
+    {
+      enable = true;
+      layout = "us";
+
+      # displayManager =
+      # {
+      #   kdm.enable = true;
+      # };
+      #
+      # desktopManager =
+      # {
+      #   gnome3.enable = true;
+      #   default = "gnome3";
+      # };
+
+      windowManager.xmonad =
+      {
+        enable = true;
+        enableContribAndExtras = true;
+        extraPackages = haskellPackages: [
+          haskellPackages.xmonad-contrib
+          haskellPackages.xmonad-extras
+          haskellPackages.xmonad
+        ];
+      };
+      windowManager.default = "xmonad";
+    };
+  };
 
   users.extraUsers.alice =
   { isNormalUser = true;
@@ -28,4 +83,3 @@
     extraGroups = [ "wheel" "users" "vboxsf" ];
   };
 }
-
